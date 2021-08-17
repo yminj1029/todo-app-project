b948ceac1b5bd28e329ce9b234a75cddd87d984c
MinjiYun <minjiyun204@gmail.com>
2021-08-17 17:02:42 +0900

    url수정

--- back/src/account/api/urls.py
+++ back/src/account/api/urls.py
@@ -3,7 +3,5 @@ from account.api.views import UserCreateViewSet
 
 urlpatterns = [
     path('join/', UserCreateViewSet.as_view()),
-    path('rest-auth/', include('rest_auth.urls')),
-
     # path('login/', include('rest_framework.urls')),
 ]
--- back/src/myapp/urls.py
+++ back/src/myapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
+    path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/', include('account.api.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('todos.api.urls'))
